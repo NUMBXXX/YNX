c77dfd72b1fac1c8cd5b918cb86b8d1f324385ec
ZhangLinyu <3296446003@qq.com>
2023-08-18 19:44:00 +0800

    添加了YNX项目

---
+++ src/YNX_NEW/src/appinventor/ai_724792995t_ti/YNX_NEW/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"223","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"30","AlignHorizontal":"3","AlignVertical":"2","AppName":"YNX_NEW","BackgroundImage":"4e57c2c43f2b9c5854ae9697c0d775d.jpg","CloseScreenAnimation":"zoom","ScreenOrientation":"portrait","Title":"Screen1","Uuid":"0","$Components":[{"$Name":"\u6807\u7b7e1","$Type":"Label","$Version":"5","FontBold":"True","FontSize":"45","HasMargins":"False","Width":"-2","Text":"\u533b\u5b81\u5fc3","TextAlignment":"1","TextColor":"&HFFFF0000","Uuid":"1507289473"},{"$Name":"\u6309\u94ae1","$Type":"Button","$Version":"7","BackgroundColor":"&HFFFFFFFF","Text":"\u5f00\u59cb\u6d4b\u8bd5","Uuid":"-1807219753"}]}}
+|#
